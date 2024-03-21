5e6a74d6e244c23d5b8a9d382ed3cf4ade983b66
Madkodevian <tatianarh97@gmail.com>
2024-03-21 10:30:54 +0000

    laravel con vue.js prueba1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'
+import BookList from '@/components/BookList.vue'
+
+//Crear aplicacion
+createApp(HelloWorld).mount("#app");
